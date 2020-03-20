f1b24f2f505effe2e349684098036115b50ad13f
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-20 17:42:00 -0600

    renaming to app.py and adding index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3.7
-from flask import Flask, jsonify  # jsonify in lower case because its a method
+from flask import Flask, jsonify, request  # jsonify in lower case because its a method
 
 # JSON is a Python's Dictionary text/string 
 stores = [
@@ -31,11 +31,8 @@ def create_store():
 def get_store(name):
     for store in stores:
         if store['Name'] == name:
-            return name
-        else:
-            return "Error, store not found"
-    pass
-
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store', methods=['GET'])
@@ -44,13 +41,25 @@ def get_stores():
 
 
 @app.route('/store/<string>', methods=['POST'])
-def create_item_in_store(name: str):
-    pass
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 
 app.run(port=5000)
