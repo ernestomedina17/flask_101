3ae8e19e2aaeadc0979d903b93e5a2a44df92d35
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-20 16:37:45 -0600

    fixing quotes

--- mystore.py
+++ mystore.py
@@ -1,4 +1,7 @@
+#!/usr/local/bin/python3.7
+from flask import Flask, jsonify   # jsonify in lower case because its a method
 
+# JSON is a Python's Dictionary text/string 
 stores = [
     {
 	'Name': 'My Wonderful Store',
@@ -6,26 +9,29 @@ stores = [
 	    {
 		'Name': 'My item',
 		'Price': 15.99
-    }
-]
+            }
+        ]
     }
 ]
 
-# JSON is a Python's Dictionary text/string 
-from flask import Flask, jsonify   # lower case because is a method
 app = Flask(__name__)	
+
 @app.route('/store', methods=['POST'])
 def create_store():
     pass
+
 @app.route('/store/<string:name>', methods=['GET'])
 def get_store(name):
     pass 
+
 @app.route('/store', methods=['GET'])
 def get_stores():
     return jsonify({'stores': stores}) # gives back the list of dictionaries
+
 @app.route('/store/<string>', methods=['POST'])
 def create_item_in_store(name: str):
     pass 
+
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
     pass 
