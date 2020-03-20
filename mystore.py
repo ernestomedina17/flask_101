b91a463238186d4742871dc7b1a2d1212682e958
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-20 17:18:30 -0600

    implementing methods, and adding .gitignore

--- mystore.py
+++ mystore.py
@@ -1,39 +1,56 @@
 #!/usr/local/bin/python3.7
-from flask import Flask, jsonify   # jsonify in lower case because its a method
+from flask import Flask, jsonify  # jsonify in lower case because its a method
 
 # JSON is a Python's Dictionary text/string 
 stores = [
     {
-	'Name': 'My Wonderful Store',
-	'Items': [
-	    {
-		'Name': 'My item',
-		'Price': 15.99
+        'Name': 'My Wonderful Store',
+        'Items': [
+            {
+                'Name': 'My item',
+                'Price': 15.99
             }
         ]
     }
 ]
 
-app = Flask(__name__)	
+app = Flask(__name__)
 
-@app.route('/store', methods=['POST'])
+
 def create_store():
-    pass
+    request_data = request.get_json()  # Get the data from the request as a Dictionary
+    new_store = {
+        'name': request_data['name'],   # Store name
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
 
 @app.route('/store/<string:name>', methods=['GET'])
 def get_store(name):
-    pass 
+    for store in stores:
+        if store['Name'] == name:
+            return name
+        else:
+            return "Error, store not found"
+    pass
+
+
 
 @app.route('/store', methods=['GET'])
 def get_stores():
-    return jsonify({'stores': stores}) # gives back the list of dictionaries
+    return jsonify({'stores': stores})  # gives back the list of dictionaries
+
 
 @app.route('/store/<string>', methods=['POST'])
 def create_item_in_store(name: str):
-    pass 
+    pass
+
 
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-    pass 
+    pass
 
-app.run(port=5000) 
+
+app.run(port=5000)
