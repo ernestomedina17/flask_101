888e5ab0e5e5ada698afd35f46765811761d4eeb
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-23 17:37:02 -0600

    sqllite3

--- app3.py
+++ app3.py
@@ -4,3 +4,21 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 create_table = "CREATE TABLE users (id int, username text, password text)"
 cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_user_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_user_query, user)
+
+users = [
+    (2, 'rolf', 'asdf'),
+    (3, 'anne', 'xyz')
+]
+cursor.executemany(insert_user_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+connection.close()
+
