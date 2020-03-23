5c2b4ba5b4c2c3f41efa9515256f16a97171bbd1
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-23 13:38:44 -0600

    api get and post using a class

--- app2.py
+++ app2.py
@@ -3,25 +3,22 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app) # no need for jsonify anymore
 
+items = []  # in memory DB
 
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404 # not found
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201  # Created, 202 is Accepted but still creating it may take a long time
 
-
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()  # Get the data from the request as a Dictionary
-    new_store = {
-        'name': request_data['name'],  # Store name
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-
-
-
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
