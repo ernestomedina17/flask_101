4636d7c8e2f93e24be92b4c1524c22a3245068a1
ernestomedina17 <ernestomedina17@gmail.com>
2020-03-23 14:22:10 -0600

    replacing for loop with filter() and next()

--- app2.py
+++ app2.py
@@ -10,13 +10,11 @@ items = []  # in memory DB
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404 # not found
+        item = next(filter(lambda x: x['name'] == name, items), None) # If no next item, then None
+        return {'item': item}, 200 if item else 404 # if item exists 200, if not 404 not found
 
-    def post(self, name):         #(silent=True) returns None
-        data = request.get_json() #(force=True) When content type is not set, don't look at the header
+    def post(self, name):
+        data = request.get_json() #(force=True) don't look at the header, #(silent=True) returns None
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201  # Created, 202 is Accepted but still creating it may take a long time
