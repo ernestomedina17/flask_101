09b48fc7327b92a7577f1043512e98276975a4ef
Ernesto Eduardo Medina Nun±e <ernestomedina17>
2020-03-24 13:38:45 -0600

    Adding setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+python -m pip install --upgrade pip
+pip install virtualenv
+virtualenv venv --python=python3.8.2
+venv\Scripts\activate
+pip install -r requirements.txt
+git config --global --edit 
+git config credential.helper store
